8eb6d11d02b0000f19bc1688b388be5c8e2e38f9
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2022-12-31 00:36:46 +0600

    Add files via upload

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-import os, sys 
-print ("\t\033[1;35mChecking for Update")
-os.system("git pull")
-try:
-    __import__("bot").main()
-          baner()
-    
+import os,platform
+os.system('clear')
+print('\033[1;36m[•] \033[1;32mChecking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+    os.system('xdg-open https://facebook.com/groups/590005482506415/')
+    import bot
+    bot.baner()
