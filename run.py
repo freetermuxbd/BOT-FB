a07a0c0043754bfdc71a89c173dbef544c919782
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2022-12-31 09:50:26 +0600

    Update run.py

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/590005482506415/')
     import bot
-    bot.baner()
+    bot.menu()
